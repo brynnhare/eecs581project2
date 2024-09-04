67cd7cc100f8d460e66a4d08e5d6d446d094e26b
brynnhare <brynnhare88@gmail.com>
2024-09-04 17:37:29 -0500

    prologue comments

--- src/game.py
+++ src/game.py
@@ -1,3 +1,10 @@
 #! python3
 # main game loop and core logic
-# test will delete
+'''
+Program: Battleship
+Description: This program will be a functional two player game of battleship. This
+    file contains the main game loop and core logic.
+Output: A game of battleship.
+Authors: Brynn Hare, Micah Borghese, Katelyn Accola, Nora Manolescu, and Kyle Johnson
+Creation date: 9/4/2024
+'''
