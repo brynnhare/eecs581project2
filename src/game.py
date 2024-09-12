0df48857dd3fb16ce1834dd3f6e921f19915dcf2
Micah Borghese <micahborghese@gmail.com>
2024-09-12 14:54:09 -0500

    improvements to functionality of a pygame board -- not being used

--- src/game.py
+++ src/game.py
@@ -14,14 +14,37 @@ from gui import Board, Colors, Ships
 
 WIDTH, HEIGHT = 1150, 600 # set screen dimensions
 
-player1_board = Board(WIDTH - 150, HEIGHT, board_name="Player 1") # create the board for Player 1
-player2_board = Board(WIDTH - 150, HEIGHT, board_name="Player 2") # create the board for Player 2
+player1_board = Board(WIDTH - 350, HEIGHT, board_name="Player 1") # create the board for Player 1
+player2_board = Board(WIDTH - 350, HEIGHT, board_name="Player 2") # create the board for Player 2
 player1_ships = Ships(player1_board.surface, player1_board.square_size) # represents the available ships for Player 1
 player2_ships = Ships(player2_board.surface, player2_board.square_size) # represents the available ships for Player 2
 
 class Game:
     def __init__(self):
-        pass
+        self.ship_sizes = ['1x1', '1x2', '1x3', '1x4']
+        self.player_boards = [player1_board, player2_board]
+        self.player_ships = [player1_ships, player2_ships]
+        self.player_focus = 0
+        self.draw_boards()
+
+        for ship_size in self.ship_sizes:
+            text = f'Player {str(self.player_focus + 1)}: Click where you want your {ship_size}-long ship to be:'
+            #text = 'Click again to rotate a ship, or elsewhere if ready.'
+            self.player_boards[self.player_focus].show_text(text)
+
+            x, y = self.get_input()
+            if x is not None and y is not None:
+                #ship = Ship(x, y, direction, self.ship_to_place)
+                ship = [("A", 3)]
+                if self.player_ships[self.player_focus].is_valid(ship):
+                    ship_location = f"get_{ship_size}"
+                    print("SHIP LOCATION:", ship_location)
+                    ship_location(ship)
+                else:
+                    break
+
+        pygame.display.flip()
+        pygame.time.Clock().tick(60)
     
     def draw_boards(self):
         player1_board.draw()
@@ -33,11 +56,35 @@ class Game:
 
         screen.blit(player1_board.surface, (0,0))
         screen.blit(player2_board.surface, ((WIDTH + 50) // 2, 0)) 
+    
+    def get_input(self):
+        """
+        Converts mouse click events into board corrdinates, for any input
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Game.close()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                return x, y
+        return None, None
 
     def update(self):
-        screen.fill(Colors().get_white())
-        self.draw_boards()
+        if self.player_focus == 0:
+            self.player_boards[0].show_board()
+        else:
+            self.player_boards[1].cover_board()
+        if self.player_focus == 1:
+            self.player_boards[1].show_board()
+        else:
+            self.player_boards[0].cover_board()
+        
         pygame.display.update()
+
+    @classmethod
+    def close(cls):
+        pygame.display.quit()
+        pygame.quit()
 
 class AssignShips: # I, Brynn, can do this class?
     #class that handles 1b; assigning the correct number of ships to a user
@@ -105,16 +152,12 @@ if __name__ == '__main__':
     running = True # this is the 'game loop' that will be True until the game is closed/terminated
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT)) # create screen
+    screen.fill(Colors().get_white())
     pygame.display.set_caption("Battleship")
 
-
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-
         game = Game()
         game.update()
+        #game.close()
 
     pygame.quit()
--- src/gui.py
+++ src/gui.py
@@ -22,19 +22,25 @@ class Colors:
     def get_blue(self):
         return BLUE
 
-# fonts -- just a default font for now
-FONT = pygame.font.Font(pygame.font.get_default_font(), 24)
-
 class Board:
     def __init__(self, WIDTH, HEIGHT, board_name):
         self.board_name = board_name
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
-        self.rows = 11
+        self.rows = 12
         self.cols = 11
         self.square_size = WIDTH // 20
         self.surface = pygame.Surface((self.cols * self.square_size, self.rows * self.square_size))  # create the board as a pygame surface
+        self.cover_surface = pygame.Surface((self.cols * self.square_size, self.rows * self.square_size))
+        self.FONT = pygame.font.Font(pygame.font.get_default_font(), 24) # fonts -- just a default font for now
 
+    def show_text(self, text):
+        """
+        Displays text on the screen
+        """
+        label = self.FONT.render(text, True, BLACK)
+        self.surface.blit(label, (self.WIDTH // 4, 100))
+    
     def draw_grid(self):
         for row in range(self.rows):
             for col in range(self.cols):
@@ -42,22 +48,29 @@ class Board:
 
     def draw_labels(self):
         # Row numbers (1-10)
-        for row in range(1, self.rows+1):
-            label = FONT.render(str(row + 1), True, BLACK)
+        for row in range(1, self.rows-1):
+            label = self.FONT.render(str(row), True, BLACK)
             self.surface.blit(label, (5, row * self.square_size + self.square_size // 3 + 50))
         
         # Column letters (A-J)
         for col in range(1, self.cols+1):
-            label = FONT.render(chr(64 + col), True, BLACK)
+            label = self.FONT.render(chr(64 + col), True, BLACK)
             self.surface.blit(label, (col * self.square_size + self.square_size // 3, 5 + 50))
 
     def draw(self):
+        pygame.init()
         self.surface.fill(WHITE) # clear the surface before drawing
 
-        label = FONT.render(self.board_name, True, (0, 0, 0))
+        label = self.FONT.render(self.board_name, True, (0, 0, 0))
         self.surface.blit(label, (self.WIDTH // 4, 10))
         self.draw_grid()
         self.draw_labels()
+
+    def cover_board(self):
+        self.cover_surface.fill(Colors().get_blue())
+
+    def show_board(self):
+        self.cover_surface.fill((0,0,0))
 
 class Ships:
     def __init__(self, surface, square_size):
@@ -66,6 +79,29 @@ class Ships:
         """
         self.surface = surface
         self.square_size = square_size
+        self.cur_ships = []
+    
+    def is_valid(self, ship):
+        """
+        Checks whether a ship would be a valid placement on the board
+        """
+        for x, y in ship:
+            if x <= "A" or y < 0 or x >= "J" or y >= 11:
+                return False
+        for otherShip in self.cur_ships:
+            if self.ships_overlap(ship, otherShip):
+                return False
+        return True
+    
+    def ships_overlap(self, ship1, ship2):
+        """
+        Checks whether two ships have an overlap
+        """
+        for ship1_coord in ship1.coordinate_list:
+            for ship2_coord in ship2.coordinate_list:
+                if ship1_coord == ship2_coord:
+                    return True
+        return False
 
     def get_1x1(self, col, row):
         """
@@ -80,7 +116,9 @@ class Ships:
         row -= 1
         pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size))
         pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size), 3)
-        return [(col, row)]
+        ship = [(col, row)]
+        self.cur_ships.append(ship)
+        return ship
 
     def get_1x2(self, col, row, orientation):
         """
@@ -96,11 +134,15 @@ class Ships:
         if orientation == "horizontal":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size * 2, self.square_size))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size * 2, self.square_size), 3)
-            return [(col, row), (col + 1, row)]
+            ship = [(col, row), (col + 1, row)]
+            self.cur_ships.append(ship)
+            return ship
         elif orientation == "vertical":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 2))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 2), 3)
-            return [(col, row), (col, row + 1)]
+            ship = [(col, row), (col, row + 1)]
+            self.cur_ships.append(ship)
+            return ship
     
     def get_1x3(self, col, row, orientation):
         """
@@ -116,11 +158,16 @@ class Ships:
         if orientation == "horizontal":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size * 3, self.square_size))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size * 3, self.square_size), 3)
-            return [(col, row), (col + 1, row), (col + 2, row)]
+            ship = [(col, row), (col + 1, row), (col + 2, row)]
+            self.cur_ships.append(ship)
+            return ship
+        
         elif orientation == "vertical":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 3))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 3), 3)
-            return [(col, row), (col, row + 1), (col, row + 2)]
+            ship = [(col, row), (col, row + 1), (col, row + 2)]
+            self.cur_ships.append(ship)
+            return ship
     
     def get_1x4(self, col, row, orientation):
         """
@@ -136,11 +183,15 @@ class Ships:
         if orientation == "horizontal":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size * 4, self.square_size))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size * 4, self.square_size), 3)
-            return [(col, row), (col + 1, row), (col + 2, row), (col + 3, row)]
+            ship = [(col, row), (col + 1, row), (col + 2, row), (col + 3, row)]
+            self.cur_ships.append(ship)
+            return ship
         elif orientation == "vertical":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 4))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 4), 1)
-            return [(col, row), (col, row + 1), (col, row + 2), (col, row + 3)]
+            ship = [(col, row), (col, row + 1), (col, row + 2), (col, row + 3)]
+            self.cur_ships.append(ship)
+            return ship
     
     def get_1x5(self, col, row, orientation):
         """
@@ -156,24 +207,31 @@ class Ships:
         if orientation == "horizontal":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size * 5, self.square_size))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size * 5, self.square_size), 3)
-            return [(col, row), (col + 1, row), (col + 2, row), (col + 3, row), (col + 4, row)]
+            ship = [(col, row), (col + 1, row), (col + 2, row), (col + 3, row), (col + 4, row)]
+            self.cur_ships.append(ship)
+            return ship
         elif orientation == "vertical":
             pygame.draw.rect(self.surface, BLACK, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 5))
             pygame.draw.rect(self.surface, GREEN, (col * self.square_size, row * self.square_size + 50, self.square_size, self.square_size * 5), 3)
-            return [(col, row), (col, row + 1), (col, row + 2), (col, row + 3), (col, row + 4)]
+            ship = [(col, row), (col, row + 1), (col, row + 2), (col, row + 3), (col, row + 4)]
+            self.cur_ships.append(ship)
+            return ship
     
     def remove_ship(self, ship):
         """ 
         Removes a ship from the board.
         ship: list of tuples representing the squares the ship occupies (see return value of get_1x1, get_1x2, etc.)
         """
-        for col, row in ship:
-            # Convert (col, row) back to pixel positions
-            x = col * self.square_size
-            y = row * self.square_size + 50
+        if ship in self.cur_ships:
+            for col, row in ship:
+                # Convert (col, row) back to pixel positions
+                x = col * self.square_size
+                y = row * self.square_size + 50
 
-            # Fill the grid square with the board background color (white in this case)
-            pygame.draw.rect(self.surface, Colors().get_white(), (x, y, self.square_size, self.square_size))
+                # Fill the grid square with the board background color (white in this case)
+                pygame.draw.rect(self.surface, Colors().get_white(), (x, y, self.square_size, self.square_size))
 
-            # Redraw the grid lines for the erased cell
-            pygame.draw.rect(self.surface, Colors().get_blue(), (x, y, self.square_size, self.square_size), 1)
+                # Redraw the grid lines for the erased cell
+                pygame.draw.rect(self.surface, Colors().get_blue(), (x, y, self.square_size, self.square_size), 1)
+        else:
+            print("Ship not found!")
